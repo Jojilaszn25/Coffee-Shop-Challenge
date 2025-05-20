3eeb6543be99e9581310b898379ad660048d2a15
george <george.benedict@student.moringaschool.com>
2025-05-20 15:03:58 +0300

    new changes

---
+++ customer.py
@@ -0,0 +1,24 @@
+from order import Order
+
+class Customer:
+    def __init__(self, name):
+        self.name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Name must be a string")
+        if not (1 <= len(value) <= 15):
+            raise ValueError("Name length must be 1 to 15 characters")
+        self._name = value
+
+    def orders(self):
+        return [order for order in Order.all_orders if order.customer == self]
+
+    def coffees(self):
+        unique_coffees = set(order.coffee for order in self.orders())
+        return list(unique_coffees)
